61eb3dee6344e838a5699834c4f8fabfda3ace23
Ahmet-Kirmizi <ahmetk5113@hotmail.com>
2023-03-04 01:13:12 +0200

    server.js get role by id needs fix

--- commands/server.js
+++ commands/server.js
@@ -1,9 +1,16 @@
-const {SlashCommandBuilder} = require('@discordjs/builders');
-
+// variables
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('server').setDescription('provides info about the server'),
-    async execute(interaction){
-        await interaction.reply(`this server's name is ${interaction.guild.name} and has ${interaction.guild.memberCount} members`);
-}
-}
+  data: new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("provides info about the server"),
+  async execute(interaction) {
+    const userID = interaction.member.id;
+    console.log(interaction.guild.roles.cache.get("515174489747619853"));
+    let myRole = interaction.guild.roles.cache.get(userID);
+    await interaction.reply(
+      `this server's name is ${interaction.guild.name} and has ${interaction.guild.memberCount} members and your role is ${myRole} and your username is ${interaction.member.user.username}`
+    );
+  },
+};
