80976a137b7fd36181a2319ad96bb61e6bf5fef4
Ahmet-Kirmizi <ahmetk5113@hotmail.com>
2023-03-19 22:33:48 +0200

    fixed bug in newstr

--- commands/newstr.js
+++ commands/newstr.js
@@ -9,19 +9,20 @@ module.exports = {
     .setName("newstr")
     .setDescription("Get the latest news from turkiye!"),
   async execute(interaction) {
+    let rand =  getRandomInt(3);
     fetch(
       "https://newsdata.io/api/1/news?apikey=pub_191180c90e2c4e8d0e24038ce8f70e2a58066&country=tr"
     )
       .then((response) => response.json())
       .then((json) =>
         interaction.reply(
-          json.results[getRandomInt(3)].title +
+          json.results[rand].title +
             " " +
             " details:" +
-            json.results[getRandomInt(3)].content +
+            json.results[rand].content +
             " " +
             " link:" +
-            json.results[getRandomInt(3)].link
+            json.results[rand].link
         )
       )
       .catch((error) => console.error(error));
